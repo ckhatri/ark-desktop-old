a65389771ff6727c0203fb8604653ea23745843e
ckhatri <chirag.k.khatri@gmail.com>
2017-12-18 17:50:11 -0600

    adds time constant

--- client/app/src/constants/constants.js
+++ client/app/src/constants/constants.js
@@ -1,7 +1,20 @@
 ;(function () {
   'use strict'
 
+  const ONE_SECOND = 1000
+  const ONE_MINUTE = ONE_SECOND * 60
+  const ONE_HOUR = ONE_MINUTE * 60
+  const ONE_DAY = ONE_HOUR * 24
+  const ONE_YEAR = ONE_DAY * 365
+
   angular.module('arkclient.constants')
     // 1 ARK has 100000000 "arktoshi"
     .constant('ARKTOSHI_UNIT', Math.pow(10, 8))
+    .constant('TIME', {
+      ONE_SECOND: ONE_SECOND,
+      ONE_MINUTE: ONE_MINUTE,
+      ONE_HOUR: ONE_HOUR,
+      ONE_DAY: ONE_DAY,
+      ONE_YEAR = ONE_YEAR
+    })
 })()
